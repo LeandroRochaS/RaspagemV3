87f07d130d179521c70eb63c60427b3739d9e8a0
Leandro Rocha Santos <lerocha644@gmail.com>
2024-03-05 21:19:35 -0300

    Atualizado intervalo de tempo de execução

--- obj/Debug/net6.0/RaspagemV3.AssemblyInfo.cs
+++ obj/Debug/net6.0/RaspagemV3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RaspagemV3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba7e5887e87e0f31184c95339b8480c79dcd20fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("RaspagemV3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RaspagemV3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
